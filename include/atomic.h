dd14d29144c64f1408848cac15d9fe7911dbd34b
hyam <hyam191018@gmail.com>
2023-03-18 00:31:21 +0800

    update GPT

--- include/atomic.h
+++ include/atomic.h
@@ -4,18 +4,17 @@
 /*
  * Author: Hyam
  * Date: 2023/03/17
- * Description: 使用spinlock
+ * Description: 使用 spinlock
  */
 
 #include <stdatomic.h>
 
 typedef struct {
-    atomic_int locked; // 使用 atomic_int 定義 locked 變數
+    atomic_int locked;  // 使用 atomic_int 定義 locked 變數
 } spinlock;
 
 /* in atomic.c */
 void spinlock_lock(spinlock* lock);
 void spinlock_unlock(spinlock* lock);
-
 
 #endif
