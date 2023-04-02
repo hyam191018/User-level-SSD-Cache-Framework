bedea04fff65cdb142e3ec28b9ae9d6fe3f545ce
hyam <hyam191018@gmail.com>
2023-04-02 16:55:38 +0800

    update test

--- test/6_spdk_test.c
+++ test/6_spdk_test.c
@@ -5,6 +5,12 @@
 #include "spdk.h"
 #include "stdinc.h"
 
+/**
+ *  Author: Hyam
+ *  Date: 2023/04/02
+ *  Description: SPDK 正向測試 寫入資料、讀出資料，multi-thread競爭
+ */
+
 static void* user_func(void* arg) {
     void* buf = alloc_dma_buffer(PAGE_SIZE);
     sprintf(buf, "I am pthread %ld.\n", pthread_self());
@@ -25,5 +31,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < user_num; i++) {
         pthread_join(user[i], NULL);
     }
+    trim_spdk(0, 8);
     exit_spdk();
 }
