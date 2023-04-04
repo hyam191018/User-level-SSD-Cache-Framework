d9598a9bce9401a13c1f5467fda16c615ab16000
hyam <hyam191018@gmail.com>
2023-04-04 23:47:08 +0800

    update rename

--- test/6_submit_pio_test.c
+++ test/6_submit_pio_test.c
@@ -12,31 +12,39 @@
  *  Description: 壓力測試 admin submit pio (read only)
  */
 
-#define ROUND 10000000  // submit次數
-#define EXCEPT 50       // 期望的 hit ratio
+// randwrite 100w  28s no mg
+// randread  100w 110s no mg
+// randread  100w  135s mg 100ms 1% hit ratio
+// randwrite 100w   42s mg 100ms 0.32% hit ratio
+
+#define ROUND 1000000  // submit次數
+#define EXCEPT 50      // 期望的 hit ratio
 #define FILE_NAME "testfile"
 const unsigned long long MAX_PAGE_INDEX =
     (CACHE_BLOCK_NUMBER * CACHE_BLOCK_SIZE * 100ull) / (1024 * EXCEPT * 4ull);
 
+void* buffer;
 // 隨機4K讀寫
 static void send_pio(void) {
     unsigned page_index = rand() % MAX_PAGE_INDEX;
-    operate operation = rand() % 2 ? READ : WRITE;
-    void* buffer = alloc_dma_buffer(PAGE_SIZE);
+    operate operation = WRITE;
+    if (operation == WRITE) {
+        memset(buffer, 'A' + rand() % 25, PAGE_SIZE);
+    }
     unsigned pio_cnt = 1;
     struct pio* head = create_pio(FILE_NAME, 0, page_index, operation, buffer, pio_cnt);
     submit_pio(head);
     free_pio(head);
-    free_dma_buffer(buffer);
 }
 
 int main(int argc, char* argv[]) {
     // 開啟 udm-cache
+    force_exit_udm_cache();
     printf("%d init rc = %d\n", getpid(), init_udm_cache());
-
+    buffer = alloc_dma_buffer(PAGE_SIZE);
     for (int i = 0; i < ROUND; i++)
         send_pio();
-
+    free_dma_buffer(buffer);
     // 關閉 udm-cache
     info_udm_cache();
     printf("%d exit rc = %d\n", getpid(), exit_udm_cache());
