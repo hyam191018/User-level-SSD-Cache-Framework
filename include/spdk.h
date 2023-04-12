924d5dab95a99804954423beb6b17fc8cdbeefb7
hyam <hyam191018@gmail.com>
2023-04-13 01:56:03 +0800

    update comment

--- include/spdk.h
+++ include/spdk.h
@@ -2,9 +2,9 @@
 #define SPDK_H
 
 /**
- *  Author: Hyam
- *  Date: 2023/03/28
- *  Description: 使用SPDK存取 cache device
+ *  @author Hyam
+ *  @date 2023/03/28
+ *  @brief 使用SPDK存取 cache device
  */
 
 #include "config.h"
@@ -16,27 +16,29 @@
 typedef enum { IO_READ_QUEUE, IO_WRITE_QUEUE, MG_READ_QUEUE, MG_WRITE_QUEUE } queue_type;
 
 /**
- * Description: Init spdk, find controller > create namespace > create qpair
- * Return:  Retrun 0, if success
- *          Retrun 1, if fail
+ * @brief Init spdk, find controller > create namespace > create qpair
+ * @return 0, if success
+ *         non-zero, if fail
  */
 int init_spdk(void);
 
 /**
- * Description: Cleanup
- * Return:  No return value
+ * @brief Cleanup
+ * @return No return value
  */
 void exit_spdk(void);
 
 /**
- * Description: Get device info, must init spdk first
- * number of lba Return:  No return value
+ * @brief Get device info, must init spdk first
+ * @param block_size - 512, 4096 bytes
+ * @param device_size - Number of lba
+ * @return No return value
  */
 void get_device_info(unsigned* block_size, unsigned long* device_size);
 
 /**
  * @brief Allocate dma buffer for spdk io
- * @param len - The size in bytes of the DMA buffer to allocate.
+ * @param len - The size in bytes of the DMA buffer to allocate
  * @return address, if success
  *         NULL, if no free space
  */
@@ -44,28 +46,40 @@ void* alloc_dma_buffer(unsigned len);
 
 /**
  * @brief Free dma buffer.
+ * @param dma_buf - Allocated by alloc_dma_buf
  * @return No return value.
  */
 void free_dma_buffer(void* dma_buf);
 
 /**
- * Description: Read from SPDK, using buffer allocated from alloc_amd_buffer
- * Return:  0, if success
- *          non-zero, if fail
+ * @brief Read from SPDK
+ * @param dma_buf - Allocated by alloc_dma_buf
+ * @param offset_block - Start lba to perform io
+ * @param num_block - length of block to read
+ * @param type - queue for different threads
+ * @return 0, if success
+ *         non-zero, if fail
  */
-int read_spdk(void* buf, unsigned long offset_block, unsigned num_block, queue_type type);
+int read_spdk(void* dma_buf, unsigned long offset_block, unsigned num_block, queue_type type);
 
 /**
- * Description: Write to SPDK, using buffer allocated from alloc_amd_buffer
- * Return:  0, if success
- *          non-zero, if fail
+ * @brief Write to SPDK
+ * @param dma_buf - Allocated by alloc_dma_buf
+ * @param offset_block - Start lba to perform io
+ * @param num_block - length of block to write
+ * @param type - queue for different threads
+ * @return 0, if success
+ *         non-zero, if fail
  */
-int write_spdk(void* buf, unsigned long offset_block, unsigned num_block, queue_type type);
+int write_spdk(void* dma_buf, unsigned long offset_block, unsigned num_block, queue_type type);
 
 /**
- * Description: Perform data trimming and mark the block as invalid
- * Return:  0, if success
- *          non-zero, if fail
+ * @brief Perform data trimming and mark the block as invalid
+ * @param offset_block - Start lba to perform io
+ * @param num_block - length of block to read
+ * @param type - queue for different threads
+ * @return 0, if success
+ *         non-zero, if fail
  */
 int trim_spdk(unsigned long offset_block, unsigned num_block, queue_type type);
 
